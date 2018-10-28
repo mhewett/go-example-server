f580f2f0a8c9f3818493e5372aaac9be0cd748fe
Micheal Hewett <241132+mhewett@users.noreply.github.com>
2018-10-27 20:26:20 -0700

    Small changes to get the first examples working

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import * as bodyParser from 'body-parser'
 import * as cors from 'cors'
 import * as express from 'express'
 import * as asyncHandler from 'express-async-handler'
-import * as fs from 'fs'
+const { exec } = require('child_process');
 
 const LANG_SERVER_PORT = '8844';
+let DEBUG = false;
 
+/*
 interface TextDocument {
     uri: string
     text: string
@@ -19,7 +21,9 @@ interface Position {
     line: number
     character: number
 }
+*/
 
+/*
 async function makeGQLService(text: string): Promise<{ path: string }> {
     return new Promise<{ path: string }>(() => {
         const path = 'schema.gql';
@@ -69,12 +73,23 @@ async function definition(doc: TextDocument, pos: Position): Promise<any> {
         }
     )
 }
+*/
+
+/**
+ * Returns a 404 error for an unsupported language, method, etc.
+ * @param res the Express response
+ * @param valueType the type of item that is not supported, e.g. 'language'
+ * @param value the value that is not supported, e.g. 'Python'
+ */
+function unsupported(res: any, valueType: string, value: string): void {
+    res.status(404).send({ error: `Unsupported ${valueType}: ${value}`});
+}
 
 /**
  * pkg can be one class or an array of them.
  *
  * @param language 'Go', 'Java', etc.
- * @param pkg Either a package name such as 'io' or an array of package names
+ * @param pkgs An array of package names
  * @param funcOrClass the name of a function or class
  * @param methodName an optional method name or function name inside a class or function
  * @param document the document being viewed
@@ -82,16 +97,21 @@ async function definition(doc: TextDocument, pos: Position): Promise<any> {
  * @param format 'html', 'text', or 'raw' with html as the default
  * @param res the response to send information to
  */
-function processRequest(language: string, pkg: string[], funcOrClass:string, methodName:string, document:string, position:string, res:any, format:string) : void {
+function processRequest(language: string, pkgs: string[], funcOrClass:string, methodName:string, document:string, position:string, res:any, format:string) : void {
 
-    console.log(`lang: ${language}, ${pkg} ${funcOrClass}`);
+    if (DEBUG) {
+        console.log(`lang: ${language}, ${pkgs} ${funcOrClass}`);
+    }
 
     const lang = language.toLowerCase();
 
     if (lang === 'go') {
-        res.send('Returning Go example');  // await hover(req.body.doc, req.body.pos))
+        returnGoExample(pkgs, funcOrClass, methodName, document, position, res, format);
     }
-    else
+    else {
+        unsupported(res, 'language', lang);
+    }
+    /*
         if (lang === 'java') {
         res.send('Returning Java example');    // await definition(req.body.doc, req.body.pos))
     }
@@ -101,9 +121,90 @@ function processRequest(language: string, pkg: string[], funcOrClass:string, met
         }
     else {
         res.send({ error: `Unknown language: ${lang}` });
-    }
+    } */
 }
 
+function formatExampleAsHtml(language: string, str: string): string {
+    // Assuming godoc-formatted output for now.
+    let formatted = `${str.replace(/\n/g, '<br>')}</h2>`;
+    formatted = formatted.replace('Example:', '<h2>Example</h2>');
+
+    return formatted;
+}
+
+/**
+ * Returns a Go coding example formatted in HTML, JSON, or raw text.
+ * The examples are from the <code>godoc</code> command <code>godoc -ex pkg function</code>.
+ * Go must be installed on the local machine.
+ *
+ * @param pkgs An array of package names
+ * @param funcOrClass the name of a function or class
+ * @param methodName an optional method name or function name inside a class or function
+ * @param document the document being viewed
+ * @param position the position in the document {line: n, column: n}
+ * @param format 'html', 'text', or 'raw' with html as the default
+ * @param res the response to send information toasync function getGoExample(pkg: string[], funcOrClass:string, methodName:string, document:string, position:string, res:any, format:string) : void {
+ */
+function returnGoExample(pkgs: string[], funcOrClass:string, methodName:string, document:string, position:string, res:any, format:string) : void {
+
+    const packages = pkgs.join(' ');
+
+    // Run the godoc command
+    const docCommand = `godoc -ex ${packages} ${funcOrClass}`;
+    if (DEBUG) {
+        console.log(docCommand);
+    }
+
+    exec(`godoc -ex ${packages} ${funcOrClass}`, (err, stdout, stderr) => {
+        if (err) {
+            res.status(500).send({error: 'Error running godoc.  Is it installed? ' + stderr});
+        } else {
+            // Find the start of the example
+            let exampleCode = '';
+            let documentation = '';
+            const funcIndex = stdout.indexOf('func');
+            const exampleIndex = stdout.indexOf('Example', funcIndex);
+            if (funcIndex >= 0) {
+                if (exampleIndex > funcIndex) {
+                    documentation = stdout.substring(funcIndex, exampleIndex);
+                } else {
+                    documentation = stdout.substring(funcIndex);
+                }
+            }
+            if (exampleIndex >= 0) {
+                exampleCode = stdout.substring(exampleIndex);
+            }
+
+            // Return the example if possible, else return the documentation
+            if (exampleCode.length > 0) {
+                res.status(200);
+                if (format === 'text') {
+                    res.type('text/plain').send(exampleCode);
+                } else if (format === 'html') {
+                    res.type('text/html').send(formatExampleAsHtml('go', exampleCode));
+                } else {
+                    res.type('application/json').send({example: exampleCode, doc: documentation});
+                }
+            } else if (documentation.length > 0) {
+                res.status(200);
+                if (format === 'text') {
+                    res.type('text/plain').send(documentation);
+                } else if (format === 'html') {
+                    res.type('text/html').send(formatExampleAsHtml('go', documentation));
+                } else {
+                    res.type('application/json').send({example: '', doc: documentation});
+                }
+            } else {
+                res.status(404).send({error: `No example available for ${pkgs.join('.')}.${funcOrClass}`});
+            }
+        }
+    });
+}
+
+/**
+ * Parses arguments, starts the server, etc.
+ * Use '-port nnnn' to change the port number.
+ */
 function main(): any {
     const app = express();
 
@@ -112,9 +213,14 @@ function main(): any {
     // Parse command-line args
     for (let i = 0; i < process.argv.length; ++i) {
         if (process.argv[i] === '-port') {
-            portnum = process.argv[i + 1];   // need error check here
+            ++i;
+            portnum = process.argv[i];   // need error check on argument length here
+        } else if (process.argv[i] === '-debug') {
+            DEBUG = true;
         }
     }
+
+    // Set up Express
     app.use(bodyParser.json({limit: '10mb'}));
     app.use(cors());
     // app.use(compression);
@@ -136,7 +242,6 @@ function main(): any {
                 req.params.document, req.params.position,
                 res, req.query.format);
         }));
-
 
     // The no-package case.  Not sure if this one will ever happen in Go or Java.
     app.get('/:lang/:function',
